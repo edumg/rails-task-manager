f72848bb4d1e69ceb769f14ba27dcd0be6e70a72
eduardo gregorio <edumgregorio@gmail.com>
2017-03-21 12:12:30 +0100

    adding status to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321104617) do
+ActiveRecord::Schema.define(version: 20170321110558) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "status"
   end
 
 end
