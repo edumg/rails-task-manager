c23187e83e95b0437a1c32950dec707f23378f82
eduardo gregorio <edumgregorio@gmail.com>
2017-03-21 16:01:43 +0100

    rails task manager solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # # List tasks
+  # get "tasks", to: "tasks#index"
+
+  # # Display the details of a task
+  # get "tasks/:id", to: "tasks#show"
+
+  # # Add a task in my ToDo list (2 requests)
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+
+  # # Edit the details of a task
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # Remove a task from my ToDo list
+  # delete "tasks/:id", to: "tasks#destroy"
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name: "Learn rails", description: "Do the flash cards", status: false)
+Task.create(name: "Client logo", description: "Design new logo", status: false)
